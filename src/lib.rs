1c4517bc08c454d0b35126b235bde1724c60e7cf
Efi MK <efi.merdler@gmail.com>
2023-07-11 14:15:52 +0000

    make it library

--- src/lib.rs
+++ src/lib.rs
@@ -19,39 +19,20 @@ pub enum OutputType {
     String,
 }
 
-#[automock]
-#[async_trait]
-pub trait GetCallerIdentity {
-    async fn get_caller_identity(
-        &self,
-    ) -> Result<
-        aws_sdk_sts::operation::get_caller_identity::GetCallerIdentityOutput,
-        SdkError<GetCallerIdentityError>,
-    >;
-}
-pub struct StsClient {
-    client: Client,
+#[derive(Debug, Parser)]
+struct Opt {
+    /// The AWS Region.
+    #[clap(short, long)]
+    region: Option<String>,
+
+    /// Which profile to use.
+    #[clap(short, long)]
+    profile: Option<String>,
 }
 
-impl StsClient {
-    pub fn new(sdk_config: &::aws_types::sdk_config::SdkConfig) -> Self {
-        Self {
-            client: Client::new(sdk_config),
-        }
-    }
-}
 
-#[async_trait]
-impl GetCallerIdentity for StsClient {
-    async fn get_caller_identity(
-        &self,
-    ) -> Result<
-        aws_sdk_sts::operation::get_caller_identity::GetCallerIdentityOutput,
-        SdkError<GetCallerIdentityError>,
-    > {
-        self.client.get_caller_identity().send().await
-    }
-}
+
+
 pub fn get_region_provider(region: Option<String>) -> RegionProviderChain {
     info!("Getting region details");
 
@@ -74,65 +55,5 @@ pub async fn get_aws_config(
     } else {
         info!("Using default profile");
         aws_config::from_env().region(region_provider).load().await
-    }
-}
-pub async fn get_caller_identity(
-    client: &impl GetCallerIdentity,
-    output_type: OutputType,
-    mut writer: impl std::io::Write,
-) -> Result<()> {
-    info!("Calling 'get_caller_identity'");
-    let response = client
-        .get_caller_identity()
-        .await
-        .with_context(|| "Failed loading AWS config details. Did you run 'aws configure' ?")?;
-
-    info!("Successful call");
-    let account_id = response.account().unwrap_or_default();
-    let user_arn = response.arn().unwrap_or_default();
-
-    info!("Output type is {:?}", output_type);
-    match output_type {
-        OutputType::String => {
-            writeln!(writer, "AccountId = {}", account_id)?;
-            writeln!(writer, "UserARN = {}", user_arn)?;
-        }
-        OutputType::Json => {
-            let result = json!({
-                "accountId": account_id,
-                "UserARN": user_arn,
-            });
-            writeln!(writer, "{}", result)?;
-        }
-    }
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use aws_sdk_sts::operation::get_caller_identity::GetCallerIdentityOutput;
-    use rstest::rstest;
-
-    #[rstest]
-    #[case(OutputType::String, "AccountId = account\nUserARN = arn\n")]
-    #[case(OutputType::Json, "{\"UserARN\":\"arn\",\"accountId\":\"account\"}\n")]
-    #[tokio::test]
-    pub async fn get_caller_identity_test(#[case] input: OutputType, #[case] expected: String) {
-        let mut result = Vec::new();
-        let mut mock = MockGetCallerIdentity::new();
-        let response = GetCallerIdentityOutput::builder()
-            .account("account")
-            .arn("arn")
-            .build();
-        mock.expect_get_caller_identity()
-            .returning(move || Ok(response.clone()));
-
-        let _ = get_caller_identity(&mock, input, &mut result).await;
-
-        let result_str = String::from_utf8(result).unwrap();
-
-        assert_eq!(&result_str, &expected.to_string());
     }
 }
